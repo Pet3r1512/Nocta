df7d77809aeb3b1cc809d87644e930e1ac706fac
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-03-10 12:43:18 +1100

    #FEAT-003: navigating with navbar (#4)
    * install clsx and tailwind-merge
    
    * ops: new pages are generated
    
    * feat: connecting classnames func
    
    * feat: new page for settings
    
    * feat: new page for analytics
    
    * feat: add href to interface Section
    
    * feat: add href for each section
    
    * fix: use a tag instead
    
    * feat: highlight current section using tanstack router pathname

--- app/components/Layout/BottomNavBar.tsx
+++ app/components/Layout/BottomNavBar.tsx
@@ -1,27 +1,35 @@
+import { useRouter } from "@tanstack/react-router";
 import { Bed, ChartPie, Settings } from "lucide-react";
 import { ReactNode } from "react";
+import { cn } from "~/utils/cn";
 
 interface Section {
   icon: ReactNode;
   name: string;
+  href: string;
 }
 
 const ICON_SIZE = 20;
 const ICON_STROKE = 2.5;
 
 export default function BottomNavBar() {
+  const route = useRouter();
+
   const sections: Section[] = [
     {
       icon: <Bed size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
       name: "Sleep",
+      href: "/",
     },
     {
       icon: <ChartPie size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
       name: "Analytics",
+      href: "/analytics",
     },
     {
       icon: <Settings size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
       name: "Settings",
+      href: "/settings",
     },
   ];
 
@@ -29,10 +37,19 @@ export default function BottomNavBar() {
     <section className="dark:bg-black bg-white shadow-2xl py-1.5 w-full fixed bottom-0 flex items-center justify-center gap-x-5">
       {sections.map((section, index) => {
         return (
-          <div key={index} className="flex flex-col gap-y-2 items-center">
+          <a
+            href={section.href}
+            key={index}
+            className={cn(
+              "flex flex-col gap-y-2 items-center",
+              route.latestLocation.pathname == section.href
+                ? "opacity-100"
+                : "opacity-65"
+            )}
+          >
             {section.icon}
             <p className="font-semibold text-xs md:text-sm">{section.name}</p>
-          </div>
+          </a>
         );
       })}
     </section>
