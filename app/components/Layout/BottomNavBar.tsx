d1fba09aed02370440c79c9d0131194df10e8081
Peter Pham <pttp15122002@gmail.com>
2025-03-10 12:42:23 +1100

    feat: highlight current section using tanstack router pathname

--- app/components/Layout/BottomNavBar.tsx
+++ app/components/Layout/BottomNavBar.tsx
@@ -1,5 +1,7 @@
+import { useRouter } from "@tanstack/react-router";
 import { Bed, ChartPie, Settings } from "lucide-react";
 import { ReactNode } from "react";
+import { cn } from "~/utils/cn";
 
 interface Section {
   icon: ReactNode;
@@ -11,6 +13,8 @@ const ICON_SIZE = 20;
 const ICON_STROKE = 2.5;
 
 export default function BottomNavBar() {
+  const route = useRouter();
+
   const sections: Section[] = [
     {
       icon: <Bed size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
@@ -33,6 +37,16 @@ export default function BottomNavBar() {
     <section className="dark:bg-black bg-white shadow-2xl py-1.5 w-full fixed bottom-0 flex items-center justify-center gap-x-5">
       {sections.map((section, index) => {
         return (
+          <a
+            href={section.href}
+            key={index}
+            className={cn(
+              "flex flex-col gap-y-2 items-center",
+              route.latestLocation.pathname == section.href
+                ? "opacity-100"
+                : "opacity-65"
+            )}
+          >
             {section.icon}
             <p className="font-semibold text-xs md:text-sm">{section.name}</p>
           </a>
