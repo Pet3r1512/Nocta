746ecc745c0f04beec9967ec55b6313133bca14b
Peter Pham <pttp15122002@gmail.com>
2025-03-11 11:13:45 +1100

    feat: add new component to sleep page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Logo from "~/components/Layout/Logo";
+import SleepTrackerClock from "~/components/Sleep/SleepTracker";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -10,8 +11,9 @@ function Home() {
     <div className="p-2">
       <div className="flex items-center">
         <Logo />
-        <h1 className="text-3xl text-primary font-bold">Nocta</h1>
+        <h1 className="text-2xl text-primary font-bold">Nocta</h1>
       </div>
+      <SleepTrackerClock />
     </div>
   );
 }
