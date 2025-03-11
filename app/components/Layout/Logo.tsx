a605a87dfa6d409ca68c22720187d6beec780bbc
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-03-11 11:22:52 +1100

    #FEAT-006: build sleep page (#6)
    * style: reduce logo's size
    
    * feat(cont): sleep calculator page
    
    * feat: add new component to sleep page
    
    * imp: rename file for better consistency
    
    * fix: wrong update path

--- app/components/Layout/Logo.tsx
+++ app/components/Layout/Logo.tsx
@@ -1,3 +1,3 @@
 export default function Logo() {
-  return <img src="/images/logo/Logo.png" alt="Logo" width={75} height={75} />;
+  return <img src="/images/logo/Logo.png" alt="Logo" width={50} height={50} />;
 }
