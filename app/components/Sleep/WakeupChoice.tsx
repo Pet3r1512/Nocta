651e5d1a21230456eba8b6fa9a24ec387c0c208f
Peter Pham <pttp15122002@gmail.com>
2025-03-12 13:34:39 +1100

    feat: drawer for choosing wakeup time

---
+++ app/components/Sleep/WakeupChoice.tsx
@@ -0,0 +1,37 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "app/components/ui/drawer";
+import { Button } from "../ui/button";
+
+export default function WakeupChoice() {
+  return (
+    <Drawer direction="right">
+      <DrawerTrigger className="bg-black dark:bg-white text-white dark:text-black rounded-2xl px-1.5 py-0.5">
+        Choose
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerClose className="w-fit">
+            <Button variant="link">{">>"}</Button>
+          </DrawerClose>
+          <DrawerTitle className="text-2xl font-bold text-primary">
+            Alarm
+          </DrawerTitle>
+          <DrawerDescription>
+            What time would you prefer to wake up?
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Confirm</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
