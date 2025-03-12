d672f973a091dac6b4867255946bdc94c5720d97
Peter Pham <pttp15122002@gmail.com>
2025-03-12 14:09:22 +1100

    feat: auto scroll to current time

--- app/components/Sleep/WakeupChoice.tsx
+++ app/components/Sleep/WakeupChoice.tsx
@@ -9,8 +9,40 @@ import {
   DrawerTrigger,
 } from "app/components/ui/drawer";
 import { Button } from "../ui/button";
+import AnimatedList from "../ui/animatedList";
 
-export default function WakeupChoice() {
+const HOURS = [
+  "00",
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+];
+
+const MINUTES = Array.from({ length: 60 }, (_, i) =>
+  i.toString().padStart(2, "0")
+);
+
+export default function WakeupChoice({ time }: { time: string }) {
   return (
     <Drawer direction="right">
       <DrawerTrigger className="bg-black dark:bg-white text-white dark:text-black text-sm rounded-lg px-1.5 py-0.5">
@@ -28,6 +60,28 @@ export default function WakeupChoice() {
             What time would you prefer to wake up?
           </DrawerDescription>
         </DrawerHeader>
+        <section className="flex ">
+          <AnimatedList
+            items={HOURS}
+            onItemSelect={(item, index) =>
+              console.log(item.padStart(2, "0"), index)
+            }
+            showGradients={true}
+            enableArrowNavigation={true}
+            displayScrollbar={true}
+            initialSelectedIndex={parseInt(time.split(":")[0], 10)}
+          />
+          <AnimatedList
+            items={MINUTES}
+            onItemSelect={(item, index) =>
+              console.log(item.padStart(2, "0"), index)
+            }
+            showGradients={true}
+            enableArrowNavigation={true}
+            displayScrollbar={true}
+            initialSelectedIndex={parseInt(time.split(":")[1], 10)}
+          />
+        </section>
         <DrawerFooter>
           <Button>Confirm</Button>
         </DrawerFooter>
