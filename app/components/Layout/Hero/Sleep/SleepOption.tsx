00a6b9b9ee6cf657e7ae2d361052db0ecba5a3b1
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-04-09 20:04:23 +1000

    #IMP-002: responsive for mobile (#11)
    * ops: define a map for rendering correct bg color for different sleep choice
    
    * feat: display correct bg colors
    
    * style: add vertical gap among elements
    
    * fix(style): hidden content at bottom
    
    * style: responsive for tablets
    
    * imp(style): improve contrast
    
    * imp(style): reduce gap on mobile

--- app/components/Layout/Hero/Sleep/SleepOption.tsx
+++ app/components/Layout/Hero/Sleep/SleepOption.tsx
@@ -11,14 +11,23 @@ export default function SleepOption({
 }: {
   sleepOption: SleepOption;
 }) {
+  const colorMap: Record<string, string> = {
+    Ideal: "bg-green-600",
+    Good: "bg-yellow-500",
+    Okay: "bg-orange-600",
+  };
   return (
-    <div className="flex items-center gap-x-5 w-full bg-primary/50 rounded-2xl px-5 py-3.5 lg:hover:bg-primary/80 transition-all duration-150 ease-linear">
+    <div className="group flex items-center gap-x-5 w-full bg-primary/60 rounded-2xl px-5 py-3.5 lg:hover:bg-primary/80 transition-all duration-150 ease-linear">
       <Bed />
-      <div className="space-y-2.5 flex-1">
+      <div className="space-y-1 md:space-y-2.5 flex-1">
         <p className="text-lg lg:text-2xl font-bold">{sleepOption.time}</p>
-        <p className="text-gray-400">{`${sleepOption.cycles} sleep cycles • 9h 15m`}</p>
+        <p className="text-gray-300 lg:group-hover:text-white transition-all duration-150 ease-linear">{`${sleepOption.cycles} sleep cycles • 9h 15m`}</p>
       </div>
-      <button className="bg-green-600 rounded-2xl px-2 py-0.5">
+      <button
+        className={`rounded-2xl px-2 py-0.5 font-semibold pb-1 ${
+          colorMap[sleepOption.type]
+        }`}
+      >
         {sleepOption.type}
       </button>
     </div>
--- app/components/Layout/Hero/index.tsx
+++ app/components/Layout/Hero/index.tsx
@@ -3,12 +3,12 @@ import Wakeup from "./Wakeup";
 
 export default function Hero() {
   return (
-    <section className="max-w-7xl mx-auto">
+    <section className="max-w-7xl mx-auto py-10 space-y-5">
       <h1 className="text-2xl lg:text-3xl font-bold text-primary text-center">
         Find Your Optimal Bedtime
       </h1>
 
-      <div className="flex flex-col lg:flex-row gap-x-5 mb-16 w-full">
+      <div className="flex flex-col lg:flex-row gap-x-5 gap-y-6 mb-16 w-full">
         <Wakeup />
         <Asleep />
       </div>
