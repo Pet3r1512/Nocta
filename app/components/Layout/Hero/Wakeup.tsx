adcf09b5de697f5cda2054d863a96c0359775996
Peter Pham <pttp15122002@gmail.com>
2025-04-15 19:44:06 +1000

    feat: update wakeup time when user has chosen

--- app/components/Layout/Hero/Wakeup.tsx
+++ app/components/Layout/Hero/Wakeup.tsx
@@ -4,13 +4,19 @@ import { useState } from "react";
 
 export default function Wakeup() {
   const [wakeUpTime, setWakeUpTime] = useState<string>("07:00");
+
   return (
     <section className="space-y-5">
       <div className="flex items-center gap-x-2">
         <Sun className="text-yellow-400" size={20} strokeWidth={2.5} />
         <p className="lg:text-lg font-semibold">I want to wake up at</p>
       </div>
-      <TimeSelector time={wakeUpTime} onChange={() => {}} />
+      <TimeSelector
+        time={wakeUpTime}
+        onChange={(time: string) => {
+          setWakeUpTime(time);
+        }}
+      />
       <p className="text-gray-400 dark:text-gray-500">
         Select your desired wake-up time, and we'll calculate the best times to
         go to sleep.
