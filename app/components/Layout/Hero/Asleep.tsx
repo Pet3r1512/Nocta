3f0a2a42c0b0f1c8d17a9638d3a44b34bf1ef039
Peter Pham <pttp15122002@gmail.com>
2025-04-15 19:56:00 +1000

    imp: dynamic rendering

--- app/components/Layout/Hero/Asleep.tsx
+++ app/components/Layout/Hero/Asleep.tsx
@@ -57,17 +57,13 @@ export default function Asleep({ wakeUpTime }: { wakeUpTime: string }) {
           Fall asleep at one of these times:
         </p>
       </div>
+
       <div className="space-y-5">
-        <SleepOption
-          sleepOption={{ time: "9:45PM", cycles: 6, type: "Ideal" }}
-        />
-        <SleepOption
-          sleepOption={{ time: "11:15PM", cycles: 5, type: "Good" }}
-        />
-        <SleepOption
-          sleepOption={{ time: "00:45AM", cycles: 4, type: "Okay" }}
-        />
+        {sleepOptions.map((option) => (
+          <SleepOption key={option.cycles} sleepOption={option} />
+        ))}
       </div>
+
       <p className="text-gray-400 dark:text-gray-500">
         These times account for the average 15 minutes it takes to fall asleep.
       </p>
