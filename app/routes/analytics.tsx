80504c5fbcb5b949bc94470949be2cba3c845b28
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-04-24 19:40:52 +1000

    #FEAT-011: coming soon pages (#13)
    * feat: coming soon for settings page
    
    * feat: coming soon for analytic page

--- app/routes/analytics.tsx
+++ app/routes/analytics.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Logo from "~/components/Layout/Logo";
 
 export const Route = createFileRoute("/analytics")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/analytics"!</div>;
+  return (
+    <div className="p-5 md:px-14 lg:px-0 mb-5">
+      <div className="flex items-center">
+        <Logo />
+        <h1 className="text-2xl text-primary font-bold">Nocta</h1>
+      </div>
+      <p className="text-center font-semibold text-lg lg:text-2xl">
+        Analytic Page is coming soon!
+      </p>
+    </div>
+  );
 }
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import Logo from "~/components/Layout/Logo";
 
-export const Route = createFileRoute('/settings')({
+export const Route = createFileRoute("/settings")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/setting"!</div>
+  return (
+    <div className="p-5 md:px-14 lg:px-0 mb-5">
+      <div className="flex items-center">
+        <Logo />
+        <h1 className="text-2xl text-primary font-bold">Nocta</h1>
+      </div>
+      <p className="text-center font-semibold text-lg lg:text-2xl">
+        Settings Page is coming soon!
+      </p>
+    </div>
+  );
 }
