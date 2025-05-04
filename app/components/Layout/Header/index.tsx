86ede9a9f196f10d4c896752c242102618112adf
Peter Pham <pttp15122002@gmail.com>
2025-05-04 13:34:36 +1000

    feat: implement a header component for reusing

---
+++ app/components/Layout/Header/index.tsx
@@ -0,0 +1,14 @@
+import ThemeToggle from "~/components/Buttons/ThemeToggle";
+import Logo from "../Logo";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between px-3.5 md:px-5 py-3 lg:px-0">
+      <div className="flex items-center">
+        <Logo />
+        <h1 className="text-2xl text-primary font-bold">Nocta</h1>
+      </div>
+      <ThemeToggle />
+    </header>
+  );
+}
