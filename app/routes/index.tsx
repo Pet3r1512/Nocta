6f4406313ce515a277ba6602d904eb65b94c2265
Peter Pham <pttp15122002@gmail.com>
2025-05-04 13:35:34 +1000

    imp: use Header component instead

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Hero from "~/components/Layout/Hero";
-import Logo from "~/components/Layout/Logo";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -9,10 +8,6 @@ export const Route = createFileRoute("/")({
 function Home() {
   return (
     <div className="p-5 md:px-14 lg:px-0 mb-5">
-      <div className="flex items-center">
-        <Logo />
-        <h1 className="text-2xl text-primary font-bold">Nocta</h1>
-      </div>
       <Hero />
     </div>
   );
