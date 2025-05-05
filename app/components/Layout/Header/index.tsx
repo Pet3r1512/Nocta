a5d5a21e0f69cc3447eb7837eefd8c0bb9102fc3
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-05-05 20:06:33 +1000

    #IMP-003: remove selected time button (#18)
    * style: setup tailwind reusable colors
    
    * feat: implement a header component for reusing
    
    * feat: add Header component to root layout
    
    * style: apply correct colors for dark and light mode
    
    * imp: use Header component instead
    
    * style: add padding x for desktop screens
    
    * imp: remove duplicated content
    
    * imp(color): add color for dark mode

--- app/components/Layout/Header/index.tsx
+++ app/components/Layout/Header/index.tsx
@@ -3,7 +3,7 @@ import Logo from "../Logo";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-3.5 md:px-5 py-3 lg:px-0">
+    <header className="flex items-center justify-between px-3.5 md:px-5 py-3">
       <div className="flex items-center">
         <Logo />
         <h1 className="text-2xl text-primary font-bold">Nocta</h1>
--- app/components/Layout/Hero/TimeSelector.tsx
+++ app/components/Layout/Hero/TimeSelector.tsx
@@ -25,7 +25,7 @@ function TimeSelector({ time, onChange }: TimeSelectorProps) {
       <div className="relative">
         <ClockIcon
           size={20}
-          className="absolute left-3 top-1/2 -translate-y-1/2 text-primary/30"
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-primary/80 dark:text-white"
         />
         {isIOS ? (
           // Custom fallback for iOS
@@ -45,12 +45,6 @@ function TimeSelector({ time, onChange }: TimeSelectorProps) {
             className="w-full bg-primary/10 border border-primary rounded-lg py-3 pl-10 pr-4 text-xl focus:outline-none focus:ring-2 focus:ring-primary"
           />
         )}
-      </div>
-      <div className="flex items-center justify-center mt-2">
-        <div className="bg-primary/80 rounded-full py-3 px-8 text-center text-2xl font-bold">
-          {hours === 0 ? "12" : hours > 12 ? hours - 12 : hours}:
-          {minutes.toString().padStart(2, "0")} {hours >= 12 ? "PM" : "AM"}
-        </div>
       </div>
     </div>
   );
