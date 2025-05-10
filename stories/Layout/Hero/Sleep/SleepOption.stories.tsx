5926ba6dba78a15b100ff886e14893145803d78c
Peter Pham <pttp15122002@gmail.com>
2025-05-11 09:55:24 +1000

    test: ideal sleep option

---
+++ stories/Layout/Hero/Sleep/SleepOption.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SleepOption from "../../../../app/components/Layout/Hero/Sleep/SleepOption";
+import "../../../../app/styles/app.css";
+
+const meta: Meta<typeof SleepOption> = {
+  title: "Sleep Option Component",
+  component: SleepOption,
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ maxWidth: "33.33%" }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SleepOption>;
+
+export const IdealOption: Story = {
+  args: {
+    sleepOption: {
+      time: "10:00 PM",
+      cycles: 6,
+      type: "Ideal",
+    },
+  },
+};
