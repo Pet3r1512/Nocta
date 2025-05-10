a93d8497a9e457598ed4881725033f827a41c70c
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-05-10 21:16:44 +1000

    #OPS-007: setup storybook tests (#23)
    * dist: install storybook and related tools
    
    * ops: storybook and vitest configs
    
    * chore: ignore storybook's log file
    
    * test: storybook's config
    
    * refactor: Logo component
    
    * test: simple test for logo component
    
    * fix: install playwright
    
    * imp(ci): install playwright via mircosoft github actions instead
    
    * fix(ci): reverse using exec playwright instead

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,17 @@
+import type { TestRunnerConfig } from '@storybook/test-runner';
+import { expect } from "vitest"
+
+const config: TestRunnerConfig = {
+    async postVisit(page, context) {
+        // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
+        const elementHandler = await page.$('#storybook-root');
+        if (elementHandler) {
+            const innerHTML = await elementHandler.innerHTML();
+            expect(innerHTML).toMatchSnapshot();
+        } else {
+            throw new Error("Element '#storybook-root' not found");
+        }
+    },
+};
+
+export default config;
--- app/components/Layout/Logo.tsx
+++ app/components/Layout/Logo.tsx
@@ -1,3 +1,8 @@
 export default function Logo() {
-  return <img src="/images/logo/Logo.png" alt="Logo" width={50} height={50} />;
+  return (
+    <div className="flex items-center">
+      <img src="/images/logo/Logo.png" alt="Logo" width={50} height={50} />
+      <h1 className="text-2xl text-primary font-bold">Nocta</h1>
+    </div>
+  );
 }
