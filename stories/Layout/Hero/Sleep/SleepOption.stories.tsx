1d2ec2b8f258e2ab8cae1be1038026dcad3bb9f9
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-05-11 10:53:39 +1000

    #TEST-001: sleep option test (#24)
    * test: ideal sleep option
    
    * test: good option
    
    * test: okay option
    
    * test: check correct type to be displayed in the button
    
    * chore: change time to default time

---
+++ stories/Layout/Hero/Sleep/SleepOption.stories.tsx
@@ -0,0 +1,65 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SleepOption from "../../../../app/components/Layout/Hero/Sleep/SleepOption";
+import "../../../../app/styles/app.css";
+import { within } from "@testing-library/react";
+import { expect } from "@storybook/test";
+
+const meta: Meta<typeof SleepOption> = {
+  title: "Sleep Option Component",
+  component: SleepOption,
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ maxWidth: "33.33%" }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SleepOption>;
+
+export const IdealOption: Story = {
+  args: {
+    sleepOption: {
+      time: "09:45 PM",
+      cycles: 6,
+      type: "Ideal",
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.getByText("Ideal")).toBeInTheDocument();
+  },
+};
+
+export const GoodOption: Story = {
+  args: {
+    sleepOption: {
+      time: "11:15 PM",
+      cycles: 5,
+      type: "Good",
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.getByText("Good")).toBeInTheDocument();
+  },
+};
+
+export const OkayOption: Story = {
+  args: {
+    sleepOption: {
+      time: "12:45 AM",
+      cycles: 4,
+      type: "Okay",
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.getByText("Okay")).toBeInTheDocument();
+  },
+};
