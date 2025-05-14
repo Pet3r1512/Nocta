a85ef841c11e6850936c388f1ff78340f404af02
Peter Pham <pttp15122002@gmail.com>
2025-05-14 20:47:16 +1000

    test: for useSleepTimeCalculator hook

---
+++ app/hooks/useSleepTimeCalculator.test.ts
@@ -0,0 +1,29 @@
+import { renderHook } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { useSleepTimeCalculator } from "./useSleepTimeCalculator";
+import { SleepOption } from "~/components/Layout/Hero/Sleep/SleepOption";
+
+describe("useSleepTimeCalculator test", () => {
+    it("should return 3 correct sleep times", () => {
+        const { result } = renderHook(() => useSleepTimeCalculator())
+        const sleepOptions: SleepOption[] = [
+            {
+                time: "08:45 pm",
+                cycles: 6,
+                type: "Ideal"
+            },
+            {
+                time: "10:15 pm",
+                cycles: 5,
+                type: "Good"
+            },
+            {
+                time: "11:45 pm",
+                cycles: 4,
+                type: "Okay"
+            },
+        ]
+
+        expect(result.current.sleepTimeCalculator("06:00")).toStrictEqual(sleepOptions)
+    });
+});
