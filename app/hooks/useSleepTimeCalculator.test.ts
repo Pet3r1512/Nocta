448a6641794fb420bb5ccdc9800fc856123924b0
Peter Pham <pttp15122002@gmail.com>
2025-05-14 20:50:59 +1000

    test: add new test case

--- app/hooks/useSleepTimeCalculator.test.ts
+++ app/hooks/useSleepTimeCalculator.test.ts
@@ -4,7 +4,7 @@ import { useSleepTimeCalculator } from "./useSleepTimeCalculator";
 import { SleepOption } from "~/components/Layout/Hero/Sleep/SleepOption";
 
 describe("useSleepTimeCalculator test", () => {
-    it("should return 3 correct sleep times", () => {
+    it("should return 3 correct sleep times for 06:00 AM wakeup", () => {
         const { result } = renderHook(() => useSleepTimeCalculator())
         const sleepOptions: SleepOption[] = [
             {
@@ -25,5 +25,27 @@ describe("useSleepTimeCalculator test", () => {
         ]
 
         expect(result.current.sleepTimeCalculator("06:00")).toStrictEqual(sleepOptions)
+    });
+    it("should return 3 correct sleep times for 07:30 AM wakeup", () => {
+        const { result } = renderHook(() => useSleepTimeCalculator())
+        const sleepOptions: SleepOption[] = [
+            {
+                time: "10:15 pm",
+                cycles: 6,
+                type: "Ideal"
+            },
+            {
+                time: "11:45 pm",
+                cycles: 5,
+                type: "Good"
+            },
+            {
+                time: "01:15 am",
+                cycles: 4,
+                type: "Okay"
+            },
+        ]
+
+        expect(result.current.sleepTimeCalculator("07:30")).toStrictEqual(sleepOptions)
     });
 });
