a086d2d3bc9c2527300a73511c067dd6f3f29c46
Peter Pham <pttp15122002@gmail.com>
2025-05-15 21:39:20 +1000

    test: bottom navbar

---
+++ stories/Layout/BottomNavBar.stories.tsx
@@ -0,0 +1,37 @@
+// BottomNavBar.stories.tsx
+import type { Meta, StoryObj } from "@storybook/react";
+import BottomNavBar from "../../app/components/Layout/BottomNavBar";
+import { within } from "@testing-library/react";
+import { expect } from "@storybook/test";
+
+const meta: Meta<typeof BottomNavBar> = {
+  component: BottomNavBar,
+  title: "Components/BottomNavBar",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BottomNavBar>;
+
+export const BottomNavBarStory: Story = {
+  args: {
+    pathname: "/",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(canvas.getByText("Sleep")).toBeInTheDocument();
+    expect(canvas.getByText("Analytics")).toBeInTheDocument();
+    expect(canvas.getByText("Settings")).toBeInTheDocument();
+
+    expect(canvas.getByText("Sleep").closest("a")).toHaveAttribute("href", "/");
+    expect(canvas.getByText("Analytics").closest("a")).toHaveAttribute(
+      "href",
+      "/analytics"
+    );
+    expect(canvas.getByText("Settings").closest("a")).toHaveAttribute(
+      "href",
+      "/settings"
+    );
+  },
+};
