b7db7cf4781918a12e62d73f31926c6fd47f48c3
Peter Pham <pttp15122002@gmail.com>
2025-05-16 19:26:26 +1000

    test: for bottom navbar component

--- stories/Layout/BottomNavBar.stories.tsx
+++ stories/Layout/BottomNavBar.stories.tsx
@@ -1,8 +1,9 @@
 // BottomNavBar.stories.tsx
 import type { Meta, StoryObj } from "@storybook/react";
-import BottomNavBar from "../../app/components/Layout/BottomNavBar";
-import { act, within } from "@testing-library/react";
+import { within } from "@testing-library/react";
 import { expect, userEvent } from "@storybook/test";
+import BottomNavBar from "~/components/Layout/BottomNavbar/BottomNavBar";
+import { sections } from "~/components/Layout/BottomNavbar/BottomNavbarContainer";
 
 const meta: Meta<typeof BottomNavBar> = {
   component: BottomNavBar,
@@ -15,27 +16,24 @@ type Story = StoryObj<typeof BottomNavBar>;
 
 export const BottomNavBarStory: Story = {
   args: {
-    pathname: "/",
+    sections: sections,
+    currentPathname: "/",
   },
-  //   play: async ({ canvasElement }) => {
-  //     const canvas = within(canvasElement);
+  play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+    const canvas = within(canvasElement);
 
-  //     await act(async () => {
-  //       await userEvent.click(canvas.getByText("Sleep"));
-  //     });
+    expect(canvas.getByText("Sleep")).toBeInTheDocument();
+    expect(canvas.getByText("Analytics")).toBeInTheDocument();
+    expect(canvas.getByText("Settings")).toBeInTheDocument();
 
-  //     expect(canvas.getByText("Sleep")).toBeInTheDocument();
-  //     expect(canvas.getByText("Analytics")).toBeInTheDocument();
-  //     expect(canvas.getByText("Settings")).toBeInTheDocument();
-
-  //     expect(canvas.getByText("Sleep").closest("a")).toHaveAttribute("href", "/");
-  //     expect(canvas.getByText("Analytics").closest("a")).toHaveAttribute(
-  //       "href",
-  //       "/analytics"
-  //     );
-  //     expect(canvas.getByText("Settings").closest("a")).toHaveAttribute(
-  //       "href",
-  //       "/settings"
-  //     );
-  //   },
+    expect(canvas.getByText("Sleep").closest("a")).toHaveAttribute("href", "/");
+    expect(canvas.getByText("Analytics").closest("a")).toHaveAttribute(
+      "href",
+      "/analytics"
+    );
+    expect(canvas.getByText("Settings").closest("a")).toHaveAttribute(
+      "href",
+      "/settings"
+    );
+  },
 };
