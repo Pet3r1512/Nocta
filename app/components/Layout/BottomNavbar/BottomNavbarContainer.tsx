9ba3b904cc5005b48a338e03bd313e0cb6b04ca1
Peter Pham <pttp15122002@gmail.com>
2025-05-16 19:26:14 +1000

    imp: export sections

--- app/components/Layout/BottomNavbar/BottomNavbarContainer.tsx
+++ app/components/Layout/BottomNavbar/BottomNavbarContainer.tsx
@@ -5,6 +5,24 @@ import BottomNavBar from "./BottomNavBar";
 const ICON_SIZE = 20;
 const ICON_STROKE = 2.5;
 
+export const sections = [
+  {
+    icon: <Bed size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
+    name: "Sleep",
+    href: "/",
+  },
+  {
+    icon: <ChartPie size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
+    name: "Analytics",
+    href: "/analytics",
+  },
+  {
+    icon: <Settings size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
+    name: "Settings",
+    href: "/settings",
+  },
+];
+
 export default function BottomNavbarContainer({
   pathname,
 }: {
@@ -12,23 +30,6 @@ export default function BottomNavbarContainer({
 }) {
   const route = useRouter();
   const currentPathname = pathname || route.latestLocation.pathname;
-  const sections = [
-    {
-      icon: <Bed size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
-      name: "Sleep",
-      href: "/",
-    },
-    {
-      icon: <ChartPie size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
-      name: "Analytics",
-      href: "/analytics",
-    },
-    {
-      icon: <Settings size={ICON_SIZE} strokeWidth={ICON_STROKE} />,
-      name: "Settings",
-      href: "/settings",
-    },
-  ];
 
   return <BottomNavBar sections={sections} currentPathname={currentPathname} />;
 }
