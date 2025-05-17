db45d7c47ccbfb3f8d4cef43cbb6313cd221b2c8
Peter Pham <pttp15122002@gmail.com>
2025-05-17 18:08:52 +1000

    fix: ci bug with storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,27 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import path from 'path';
+import path, { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { createRequire } from "module";
+import { createRequire } from 'module';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const require = createRequire(import.meta.url);
 
-function getAbsolutePath(value: string): any {
-  return path.join(__dirname, require.resolve(path.join(value, "package.json")));
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 const config: StorybookConfig = {
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
   stories: ['../app/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
   ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
   viteFinal: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
