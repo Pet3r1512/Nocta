e04076e7bdf8f71bf7bfc60e7e81d9d620ea6570
Peter Pham <pttp15122002@gmail.com>
2025-05-26 18:05:35 +1000

    update: path to sign in page

--- app/components/Layout/Header/Auth.tsx
+++ app/components/Layout/Header/Auth.tsx
@@ -1,7 +1,7 @@
 export default function Auth() {
   return (
     <a
-      href="/auth/signin"
+      href="/auth/sign-in"
       className="text-black dark:text-white bg-primary/65 dark:bg-primary px-4 py-2.5 rounded-2xl font-semibold lg:hover:bg-primary/90 dark:lg:hover:bg-primary/75 transition-all duration-150 ease-linear"
     >
       Sign In
