61ddbb4c149f327c830fe084e304a53f2e2b6fc3
Peter Pham <pttp15122002@gmail.com>
2025-05-27 21:05:27 +1000

    chore: hide uncompleted auth

--- app/components/Layout/Header/index.tsx
+++ app/components/Layout/Header/index.tsx
@@ -1,13 +1,13 @@
 import ThemeToggle from "~/components/Buttons/ThemeToggle";
 import Logo from "../Logo";
-import Auth from "./Auth";
+// import Auth from "./Auth";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-3.5 md:px-5 py-3">
       <Logo />
       <div className="flex items-center gap-x-5">
-        <Auth />
+        {/* <Auth /> */}
         <ThemeToggle />
       </div>
     </header>
