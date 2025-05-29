839c3033da56007cec22b590ece63bef487525ab
Peter Pham <pttp15122002@gmail.com>
2025-05-29 18:41:11 +1000

    chore: imports

--- app/hooks/useTimeSelector.test.ts
+++ app/hooks/useTimeSelector.test.ts
@@ -1,8 +1,14 @@
+import { act, renderHook } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { useTimeSelector } from "./useTimeSelector";
+
+describe("useTimeSelector hook tests", () => {
     it("should initialize correctly with given time and calls onChange function", () => {
         const onChange = vi.fn();
         renderHook(() => useTimeSelector("14:30", onChange));
         expect(onChange).toHaveBeenCalledWith("14:30");
     })
+
     it("should increase hour by 1 correctly", () => {
         const onChange = vi.fn()
         const { result } = renderHook(() => useTimeSelector("07:00", onChange))
@@ -26,6 +32,7 @@
         expect(result.current.hours).toBe(6)
         expect(onChange).toHaveBeenCalledWith("06:00")
     })
+
     it("should increase minute by 5 correctly", () => {
         const onChange = vi.fn()
         const { result } = renderHook(() => useTimeSelector("07:00", onChange))
