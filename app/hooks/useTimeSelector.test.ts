cb9335734c9764779e7fb1a974b0f282ad316847
Peter Pham <pttp15122002@gmail.com>
2025-05-29 18:40:42 +1000

    tesT: increase and decrease hour

--- app/hooks/useTimeSelector.test.ts
+++ app/hooks/useTimeSelector.test.ts
@@ -3,3 +3,26 @@
         renderHook(() => useTimeSelector("14:30", onChange));
         expect(onChange).toHaveBeenCalledWith("14:30");
     })
+    it("should increase hour by 1 correctly", () => {
+        const onChange = vi.fn()
+        const { result } = renderHook(() => useTimeSelector("07:00", onChange))
+
+        act(() => {
+            result.current.incrementHours()
+        })
+
+        expect(result.current.hours).toBe(8)
+        expect(onChange).toHaveBeenCalledWith("08:00")
+    })
+
+    it("should decrease hour by 1 correctly", () => {
+        const onChange = vi.fn()
+        const { result } = renderHook(() => useTimeSelector("07:00", onChange))
+
+        act(() => {
+            result.current.decrementHours()
+        })
+
+        expect(result.current.hours).toBe(6)
+        expect(onChange).toHaveBeenCalledWith("06:00")
+    })
