014dc9b857e50feafbc4228cc22d0a7e0ed03da8
Peter Pham <pttp15122002@gmail.com>
2025-06-03 22:48:14 +1000

    ops: enable auth via email and password

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -6,10 +6,14 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
+    emailAndPassword: {
+        enabled: true
+    },
     socialProviders: {
         google: {
             clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID as string || import.meta.env.GOOGLE_CLIENT_ID,
             clientSecret: import.meta.env.VITE_GOOGLE_CLIENT_SECRET as string || import.meta.env.GOOGLE_CLIENT_SECRET,
         },
     },
+    plugins: [reactStartCookies()],
 });
