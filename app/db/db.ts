138a90705401d0875351c5dec7d17cd974ba26bf
Peter Pham <pttp15122002@gmail.com>
2025-06-03 22:49:00 +1000

    ops: use neon serverless instead

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,5 +1,10 @@
 import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
-const db = drizzle(process.env.DATABASE_URL as string);
+const databaseUrl = import.meta.env.VITE_DATABASE_URL || import.meta.env.DATABASE_URL;
+
+const sql = neon(databaseUrl);
+
+const db = drizzle(sql);
 
 export default db
