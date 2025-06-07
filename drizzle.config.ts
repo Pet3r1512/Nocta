a46df66f643628a9bb5076f24d008e19f4f64a2a
Peter Pham <pttp15122002@gmail.com>
2025-06-07 11:53:41 +1000

    fix: missing env variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 
-const databaseUrl = process.env.VITE_DATABASE_URL
-
-if (!databaseUrl) {
-    throw new Error("DATABASE_URL environment variable is not set");
-}
+const databaseUrl = process.env.VITE_DATABASE_URL as string
 
 export default defineConfig({
     dialect: "postgresql",
