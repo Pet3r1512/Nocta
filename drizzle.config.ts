10ad97a4a783bfd45f9b3445be00d2a3a0d5adb2
Peter Pham <pttp15122002@gmail.com>
2025-06-07 11:47:29 +1000

    fix: use vite's env variable instead

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
-const databaseUrl = process.env.VITE_DATABASE_URL || process.env.DATABASE_URL;
+const databaseUrl = process.env.VITE_DATABASE_URL
+
 if (!databaseUrl) {
     throw new Error("DATABASE_URL environment variable is not set");
 }
