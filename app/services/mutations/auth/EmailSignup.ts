b37b49cc279e98b8bef8e9c5004c4e7c8ea06b09
Peter Pham <pttp15122002@gmail.com>
2025-06-09 19:43:54 +1000

    imp: export tanstack query's helpers

--- app/services/mutations/auth/EmailSignup.ts
+++ app/services/mutations/auth/EmailSignup.ts
@@ -1,14 +1,23 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, UseMutationOptions } from '@tanstack/react-query'
 import { authClient } from '~/lib/auth-client'
 
+// input
 interface EmailSignUpData {
     email: string,
     password: string,
     username: string
 }
 
-export function EmailSignup() {
-    return useMutation({
+// output
+type EmailSignupResponse = Awaited<
+    ReturnType<typeof authClient.signUp.email>
+>['data']
+
+
+export function EmailSignup(
+    options?: UseMutationOptions<EmailSignupResponse, Error, EmailSignUpData>
+) {
+    return useMutation<EmailSignupResponse, Error, EmailSignUpData>({
         mutationFn: async ({ email, password, username }: EmailSignUpData) => {
             const { data, error } = await authClient.signUp.email({ email, password, name: username })
 
@@ -17,6 +26,7 @@ export function EmailSignup() {
             }
 
             return data
-        }
+        },
+        ...options
     })
 }
